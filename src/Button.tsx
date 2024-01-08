946be62e12ab1d84d57b8d535183adbb0a095b58
jadeleonard <lukabartolomeo9@gmail.com>
2024-01-08 08:45:41 -0800

    added src folder then thanks

---
+++ src/Button.tsx
@@ -0,0 +1,16 @@
+import './css/Button.css'
+import React from 'react'
+interface props {
+   title:string
+   
+}
+const Button : React.FC<props>= ({title}) =>  {
+    return(
+        <div>
+            <a className='button'><span className='span'></span>{title}</a>
+        </div>
+    )
+}
+
+
+export default Button
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import Button from "./Button";
+
+
+
+
+
+
+
+
+
+
+export{Button};
