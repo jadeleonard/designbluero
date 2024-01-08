6be5e1f9d5a52cd878a26b426929ce506e803f56
jadeleonard <lukabartolomeo9@gmail.com>
2024-01-08 10:55:06 -0800

    added dist folder and

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+import React, { HtmlHTMLAttributes } from 'react';
+
+declare module "designbluero"{
+    const Button : React.FC<HtmlHTMLAttributes<HTHMLButtonElement>>;
+    export default Button
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Button from "./Button-label";
+
+
+
+
+export default Button
